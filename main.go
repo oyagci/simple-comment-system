01a2f945cda0f53a015cd47e8263862ff8b2a314
Oguzhan YAGCI <oyagci@pm.me>
2023-02-08 12:24:45 +0100

    Use values from database when querying comments

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -32,35 +31,6 @@ type NewComment struct {
 	TargetId    string `json:"targetId"`
 }
 
-var allComments []Comment = []Comment{
-	{
-		Id:          "Comment-kjh784fgevdhhdwhh7563",
-		TextFr:      "Bonjour ! je suis un commentaire.",
-		TextEn:      "Hi ! Im a comment.",
-		PublishedAt: "1639477064",
-		AuthorId:    "User-kjh784fgevdhhdwhh7563",
-		TargetId:    "Photo-bdgetr657434hfggrt8374",
-		Replies: []Comment{
-			{
-				Id:          "Comment-1234abcd",
-				TextFr:      "Je suis une réponse au commentaire",
-				TextEn:      "Im a reply!",
-				PublishedAt: "1639477064",
-				AuthorId:    "User-5647565dhfbdshs",
-				TargetId:    "Comment-kjh784fgevdhhdwhh7563",
-			},
-			{
-				Id:          "Comment-5678efgh",
-				TextFr:      "Je suis une autre reponse !",
-				TextEn:      "Im another reply!",
-				PublishedAt: "1639477064",
-				AuthorId:    "User-5342hdfgetrfiw789",
-				TargetId:    "Comment-kjh784fgevdhhdwhh7563",
-			},
-		},
-	},
-}
-
 func get_matching_comments(comments *[]Comment, targetId string) []Comment {
 
 	var matches []Comment
@@ -74,12 +44,63 @@ func get_matching_comments(comments *[]Comment, targetId string) []Comment {
 	return matches
 }
 
+type CommentDB struct {
+	id          string
+	textfr      string
+	texten      string
+	publishedat string
+	authorid    string
+	targetid    string
+	replies     []string
+}
+
 func get_comments(w http.ResponseWriter, r *http.Request) {
+
+	connStr := "user=postgres password=root dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer func() {
+		log.Println("Closing connection")
+		db.Close()
+	}()
 
 	w.Header().Set("Content-Type", "application/json")
 
 	vars := mux.Vars(r)
-	matchingComments := get_matching_comments(&allComments, vars["targetId"])
+
+	tx, err := db.Begin()
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer func() {
+		switch err {
+		case nil:
+			log.Println("Committing")
+			err = tx.Commit()
+		default:
+			log.Println("Rolling back")
+			tx.Rollback()
+		}
+	}()
+
+	var matchingComments []Comment
+
+	if rows, err := tx.Query("SELECT * FROM comments WHERE id = $1;", vars["targetId"]); err != nil {
+		log.Println(err)
+	} else {
+		var comment CommentDB
+		rows.Scan(&comment)
+		log.Println(rows.Columns())
+		//matchingComments = append(matchingComments, comment)
+	}
 
 	json.NewEncoder(w).Encode(matchingComments)
 }
@@ -101,7 +122,7 @@ func insert_comment_db(db *sql.DB, c Comment) {
 	tx, err := db.Begin()
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
@@ -119,12 +140,19 @@ func insert_comment_db(db *sql.DB, c Comment) {
 	stmt, err := tx.Prepare("INSERT INTO comments (id, textFr, textEn, publishedAt, authorId, targetId, replies) VALUES($1, $2, $3, TO_TIMESTAMP($4), $5, $6, $7);")
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
 	if _, err = stmt.Exec(c.Id, c.TextFr, c.TextEn, c.PublishedAt, c.AuthorId, c.TargetId, nil); err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		return
+	}
+
+	updateRepliesStmt, err := tx.Prepare("UPDATE comments SET replies = ARRAY_APPEND(replies, $1) WHERE id = $2")
+
+	if _, err = updateRepliesStmt.Exec(c.Id, c.TargetId); err != nil {
+		log.Println(err)
 		return
 	}
 
@@ -149,8 +177,6 @@ func insert_comment(c Comment) error {
 	}()
 
 	insert_comment_db(db, c)
-
-	allComments = append(allComments, c)
 
 	return nil
 }
